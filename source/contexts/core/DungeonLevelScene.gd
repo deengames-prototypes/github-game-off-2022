0adf4516e67d071b8e0c5af74c3c998d573f6bcd
3akev <3akevdev@gmail.com>
2022-11-03 20:09:19 +0100

    Player can't walk into walls

--- source/contexts/core/DungeonLevelScene.gd
+++ source/contexts/core/DungeonLevelScene.gd
@@ -1,27 +1,45 @@
 extends Node2D
 
-onready var _entities_tile_map = $Entities
+signal move_player(displacement)
 
-var _entities_tile_id = {}
+onready var _entities_tile_map = $Entities
+onready var _terrain_tile_map = $Terrain
+
+var _entities_by_name = {}
 var _player: Player = Player.new()
 
 var _player_controller = preload("res://contexts/core/servers/PlayerController.gd").new(_player)
 var _input_system = preload("res://contexts/core/servers/InputSystem.gd").new()
 
 func _ready():
-	_input_system.connect("move_player", _player_controller, "on_move_player")
+	_input_system.connect("try_move_player", self, "on_move_player")
+	connect("move_player", _player_controller, "on_move_player")
 	_player_controller.connect("player_moved", self, "on_player_moved")
 
 	add_child(_input_system)
 	add_child(_player_controller)
 
 	for tile_id in _entities_tile_map.tile_set.get_tiles_ids():
-		_entities_tile_id[_entities_tile_map.tile_set.tile_get_name(tile_id)] = tile_id
+		_entities_by_name[_entities_tile_map.tile_set.tile_get_name(tile_id)] = tile_id
 
 	_player.tile_position = Vector2(3, 7)
-	_entities_tile_map.set_cellv(_player.tile_position, _entities_tile_id["player"])
+	_entities_tile_map.set_cellv(_player.tile_position, _entities_by_name["player"])
 
 func on_player_moved(old: Vector2, new: Vector2):
 	_entities_tile_map.set_cellv(old, -1)
-	_entities_tile_map.set_cellv(new, _entities_tile_id["player"])
+	_entities_tile_map.set_cellv(new, _entities_by_name["player"])
+
+func on_move_player(displacement: Vector2):
+	if is_walkable(_player.tile_position + displacement, [_player]):
+		emit_signal("move_player", displacement)
+
+func is_walkable(tile_position: Vector2, all_entities: Array) -> bool:
+	var tile_id = _terrain_tile_map.get_cellv(tile_position)
+	var tile_name = _terrain_tile_map.tile_set.tile_get_name(tile_id)
+	if tile_name == "wall":
+		return false
+	for e in all_entities:
+		if e.tile_position == tile_position:
+			return false
+	return true
 
--- source/contexts/core/servers/InputSystem.gd
+++ source/contexts/core/servers/InputSystem.gd
@@ -1,13 +1,13 @@
 extends Node
 
-signal move_player(displacement)
+signal try_move_player(displacement)
 
 func _process(delta):
 	if Input.is_action_just_pressed("ui_up"):
-		emit_signal("move_player", Vector2.UP)
+		emit_signal("try_move_player", Vector2.UP)
 	elif Input.is_action_just_pressed("ui_right"):
-		emit_signal("move_player", Vector2.RIGHT)
+		emit_signal("try_move_player", Vector2.RIGHT)
 	elif Input.is_action_just_pressed("ui_down"):
-		emit_signal("move_player", Vector2.DOWN)
+		emit_signal("try_move_player", Vector2.DOWN)
 	elif Input.is_action_just_pressed("ui_left"):
-		emit_signal("move_player", Vector2.LEFT)
+		emit_signal("try_move_player", Vector2.LEFT)
