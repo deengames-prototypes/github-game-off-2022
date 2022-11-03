f81049f6e4ba0f2bcb606d31fbd15d835b75bba2
3akev <3akevdev@gmail.com>
2022-11-03 16:12:38 +0100

    Add dungeon scene, wiring with turbo

--- source/contexts/pregame/scenes/TitleScene.gd
+++ source/contexts/pregame/scenes/TitleScene.gd
@@ -7,15 +7,14 @@ var _gamepad_navigator = GamepadNavigator.new()
 
 func _ready():
 	add_child(_gamepad_navigator)
-	
+
 func _on_OptionsButton_pressed() -> void:
 	var options_dialog = OptionsDialog.instance()
 	add_child(options_dialog)
 	options_dialog.popup()
 
 func _on_NewGameButton_pressed():
-#	SceneManager.change_scene_to(load("res://contexts/core/CoreGameScene.tscn"), "circle")
-	pass # Replace with function body.
+	SceneManager.change_scene_to(load("res://contexts/core/DungeonLevelScene.tscn"), "circle")
 
 func _on_ContinueButton_pressed():
 	pass # Replace with function body.
