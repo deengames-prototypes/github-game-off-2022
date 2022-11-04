4c89260d1abb7cf068dfc78c6dc831ad79cd941d
3akev <3akevdev@gmail.com>
2022-11-05 00:47:17 +0100

    Player can't move into entities

--- source/contexts/core/scenes/DungeonLevelScene.gd
+++ source/contexts/core/scenes/DungeonLevelScene.gd
@@ -5,11 +5,11 @@ const PlayerController = preload("res://contexts/core/scripts/systems/PlayerCont
 onready var _entities_tile_map = $Entities
 onready var _terrain_tile_map = $Terrain
 
-onready var _player_controller = PlayerController.new(_player, _terrain_tile_map)
+onready var _player_controller = PlayerController.new(_player, _terrain_tile_map, _entities)
 
-var _entities_by_name = {}
+var _entities_by_name:Dictionary = {}
 var _player:Player = Player.new()
-var _monsters = []
+var _entities:Array = [_player]
 
 func _ready():
 	_player_controller.connect("player_moved", self, "on_player_moved")
@@ -22,10 +22,10 @@ func _ready():
 	_entities_tile_map.set_cellv(_player.tile_position, _entities_by_name["player"])
 
 	for i in range(4):
-		var monster = Slime.new()
-		monster.tile_position = Vector2(11, 8 + i)
-		_monsters.append(monster)
-		_entities_tile_map.set_cellv(monster.tile_position, _entities_by_name["slime"])
+		var slime = Slime.new()
+		slime.tile_position = Vector2(11, 8 + i)
+		_entities.append(slime)
+		_entities_tile_map.set_cellv(slime.tile_position, _entities_by_name["slime"])
 
 func on_player_moved(old_tile_position: Vector2, new_tile_position: Vector2):
 	_player.tile_position = new_tile_position
--- source/contexts/core/scripts/systems/PlayerController.gd
+++ source/contexts/core/scripts/systems/PlayerController.gd
@@ -6,11 +6,14 @@ signal player_moved(new_tile_position)
 
 var _input_system = InputSystem.new()
 var _player:Player
+# Arrays are passed by reference in GDScript. As such, this variable is always up to date
+var _all_entities:Array
 var _terrain_tile_map:TileMap
 
-func _init(player:Player, terrain_tile_map:TileMap):
+func _init(player:Player, terrain_tile_map:TileMap, all_entities:Array):
 	_player = player
 	_terrain_tile_map = terrain_tile_map
+	_all_entities = all_entities
 	_input_system.connect("try_move_player", self, "on_move_player")
 
 func _ready():
@@ -21,7 +24,7 @@ func on_move_player(displacement:Vector2):
 		push_error("Player moved %s tiles, too far" % displacement.length())
 	var old_tile_position = _player.tile_position
 	var new_tile_position = old_tile_position + displacement
-	
-	if TileUtils.is_walkable(_terrain_tile_map, new_tile_position, [_player]):
+
+	if TileUtils.is_walkable(_terrain_tile_map, new_tile_position, _all_entities):
 		emit_signal("player_moved", old_tile_position, new_tile_position)
 
