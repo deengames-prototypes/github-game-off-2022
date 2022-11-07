33a459b18811cbb8c586fb739b62dad628f3cdfa
3akev <3akevdev@gmail.com>
2022-11-08 00:45:32 +0100

    [WIP] Minion attacks slimes

--- source/contexts/core/entities/Minion.gd
+++ source/contexts/core/entities/Minion.gd
@@ -6,5 +6,8 @@ const name := 'minion'
 enum {FOLLOWING, ATTACKING, IDLE}
 
 var state = FOLLOWING
+var target = null
+
+var attack: int = 1
 var health: int = 4
 var tile_position: Vector2 = Vector2()
--- source/contexts/core/scripts/systems/MinionController.gd
+++ source/contexts/core/scripts/systems/MinionController.gd
@@ -1,5 +1,8 @@
 extends Node
 
+const ENEMY_DISCOVER_DISTANCE = 5
+const ATTACK_DISTANCE = 2
+
 signal minion_moved(minion, old_tile_position, new_tile_position)
 
 var _player: Player
@@ -11,7 +14,7 @@ func _init(player: Player, terrain_tile_map: TileMap, all_entities: Array):
 	_terrain_tile_map = terrain_tile_map
 	_all_entities = all_entities
 
-func on_player_moved(entity, old_position: Vector2, new_position:Vector2):
+func on_player_moved(entity, old_position: Vector2, new_position: Vector2):
 	for entity in _all_entities:
 		if entity is Minion:
 			take_turn(entity)
@@ -19,14 +22,26 @@ func on_player_moved(entity, old_position: Vector2, new_position:Vector2):
 func take_turn(minion: Minion):
 	match minion.state:
 		minion.FOLLOWING:
+			for enemy in get_enemies_near(minion):
+				if is_near_player(enemy):
+					minion.state = minion.ATTACKING
+					minion.target = enemy
+
+	match minion.state:
+		minion.FOLLOWING:
 			follow_player(minion)
 		minion.ATTACKING:
-			pass
+			attack_target(minion)
 		minion.IDLE:
 			pass
 
+# movement
+
 func follow_player(minion: Minion):
-	var direction := minion.tile_position.direction_to(_player.tile_position)
+	chase_entity(minion, _player)
+
+func chase_entity(minion: Minion, entity):
+	var direction := minion.tile_position.direction_to(entity.tile_position)
 
 	var displacement: Vector2
 	if direction.aspect() > 0.5:
@@ -44,3 +59,26 @@ func move_minion(minion: Minion, displacement: Vector2):
 
 	if MovementCheck.is_walkable(_terrain_tile_map, new_tile_position, _all_entities):
 		emit_signal("minion_moved", minion, old_tile_position, new_tile_position)
+
+# attacking
+
+func get_enemies_near(minion):
+	var ls := []
+	for entity in _all_entities:
+		if entity is Slime and entity.tile_position.distance_to(minion.tile_position) <= ENEMY_DISCOVER_DISTANCE:
+			ls.append(entity)
+	return ls
+
+func is_near_player(enemy):
+	return enemy.tile_position.distance_to(_player.tile_position) <= ENEMY_DISCOVER_DISTANCE
+
+func attack_target(minion):
+	if minion.tile_position.distance_to(minion.target.tile_position) > ATTACK_DISTANCE:
+		chase_entity(minion, minion.target)
+	else:
+		# TODO: move into own system
+		minion.target.health -= minion.attack
+		print(minion.target.name, minion.target.health)
+		if minion.target.health <= 0:
+			minion.target = null
+			minion.state = minion.FOLLOWING
