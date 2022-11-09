4a972ded3617c988d014a371c50ce12095347a7e
3akev <3akevdev@gmail.com>
2022-11-09 15:27:05 +0100

    Extract drawing code from main file

--- source/contexts/core/scenes/DungeonLevelScene.gd
+++ source/contexts/core/scenes/DungeonLevelScene.gd
@@ -11,16 +11,15 @@ onready var _player_controller = PlayerController.new(_player, _terrain_tile_map
 onready var _minion_system = MinionSystem.new(_player, _terrain_tile_map, _entities)
 onready var _movement_system = MovementSystem.new(_terrain_tile_map, _entities)
 
-var _entities_by_name:Dictionary = {}
 var _player:Player = Player.new()
 var _entities:Array = [_player]
 
 func _ready():
 	# Wire up signals
-	_player_controller.connect("player_moved", self, "on_entity_moved")
+	_player_controller.connect("player_moved", _entities_tile_map, "on_entity_moved")
 	add_child(_player_controller)
 
-	_movement_system.connect("entity_moved", self, "on_entity_moved")
+	_movement_system.connect("entity_moved", _entities_tile_map, "on_entity_moved")
 	add_child(_movement_system)
 
 	_player_controller.connect("player_moved", _minion_system, "on_player_moved")
@@ -29,32 +28,22 @@ func _ready():
 	_minion_system.connect("entity_died", self, "on_entity_died")
 	add_child(_minion_system);
 
-	# Draw entities on the tile map
-	for tile_id in _entities_tile_map.tile_set.get_tiles_ids():
-		_entities_by_name[_entities_tile_map.tile_set.tile_get_name(tile_id)] = tile_id
-
 	_player.tile_position = Vector2(9, 8)
-	_entities_tile_map.set_cellv(_player.tile_position, _entities_by_name[_player.name])
 
 	# Generate some slimes to kill
 	for i in range(4):
 		var slime = Slime.new()
 		slime.tile_position = Vector2(11, 8 + i)
 		_entities.append(slime)
-		_entities_tile_map.set_cellv(slime.tile_position, _entities_by_name[slime.name])
 
 	# Add the first (test) minion to follow the player around
 	var minion = Minion.new()
 	minion.tile_position = Vector2(8, 8)
-	_entities_tile_map.set_cellv(minion.tile_position, _entities_by_name[minion.name])
 	_entities.append(minion)
 
-func on_entity_moved(entity, old_tile_position: Vector2, new_tile_position: Vector2):
-	entity.tile_position = new_tile_position
-	_entities_tile_map.set_cellv(old_tile_position, TileMap.INVALID_CELL)
-	_entities_tile_map.set_cellv(new_tile_position, _entities_by_name[entity.name])
+	_entities_tile_map.draw_entities(_entities)
 
 func on_entity_died(entity):
 	_entities.erase(entity)
-	_entities_tile_map.set_cellv(entity.tile_position, TileMap.INVALID_CELL)
+	_entities_tile_map.on_entity_died(entity)
 
---
+++ source/contexts/core/scenes/level/EntityDrawer.gd
@@ -0,0 +1,20 @@
+extends TileMap
+
+var entities_by_name: Dictionary = {}
+
+func _ready():
+	# cache tile ids by name
+	for tile_id in tile_set.get_tiles_ids():
+		entities_by_name[tile_set.tile_get_name(tile_id)] = tile_id
+
+func draw_entities(entities: Array):
+	for entity in entities:
+		set_cellv(entity.tile_position, entities_by_name[entity.name])
+
+func on_entity_moved(entity, old_tile_position: Vector2, new_tile_position: Vector2):
+	entity.tile_position = new_tile_position
+	set_cellv(old_tile_position, TileMap.INVALID_CELL)
+	set_cellv(new_tile_position, entities_by_name[entity.name])
+
+func on_entity_died(entity):
+	set_cellv(entity.tile_position, TileMap.INVALID_CELL)
