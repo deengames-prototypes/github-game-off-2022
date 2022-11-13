a77b72b1af357f557904961ca2f582ddbd2bd77e
3akev <3akevdev@gmail.com>
2022-11-13 17:30:21 +0100

    Move minion vision variables to minion script

--- source/contexts/core/entities/Entity.gd
+++ source/contexts/core/entities/Entity.gd
@@ -3,7 +3,7 @@ class_name Entity
 extends Resource
 
 # read only, settable only within the owner script
-var name: String setget _set_name, _get_name
+var name: String setget _set_name
 
 var attack: int
 var health: int
@@ -11,8 +11,4 @@ var tile_position: Vector2
 
 func _set_name(val):
 	pass
-
-func _get_name():
-	return name
-
 
--- source/contexts/core/entities/Minion.gd
+++ source/contexts/core/entities/Minion.gd
@@ -4,8 +4,18 @@ extends Entity
 
 enum {FOLLOWING, ATTACKING}
 
+var discover_distance = 5 setget set_noop
+var chase_range = 2 setget set_noop
+var following_distance = 3 setget set_noop
+var retreat_distance = 7 setget set_noop
+
+func set_noop(val):
+	pass
+
 var state = FOLLOWING
 var target: Entity = null
+
+var skills: Array = []
 
 func _init(x: int, y: int):
 	name = 'minion'
